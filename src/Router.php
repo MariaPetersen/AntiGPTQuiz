4bf86ed18a493f53a9fc376266e9887a7f1886ab
Maria Petersen <mau.petersen@gmail.com>
2024-02-22 12:20:52 +0100

    change structure

---
+++ src/Router.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace antiCheat;
+
+class Router {
+    protected $routes = [];
+
+    public function addRoute($route, $controller, $action) {
+        $this->routes[$route] = ['controller' => $controller, 'action' => $action]
+    }
+}
