e9b144cd7ca8c54c74f25edebf8edc7118cfa308
Maria Petersen <mau.petersen@gmail.com>
2024-02-23 10:08:10 +0100

    fix ReceiveAnswersController

--- src/Controllers/ReceiveAnswersController.php
+++ src/Controllers/ReceiveAnswersController.php
@@ -6,21 +6,31 @@ class ReceiveAnswers {
     private $jsonString;
     private $jsonData;
 
-    public function __construct($answers) {
+    public function __construct() {
         $this->questionsPath = __DIR__ . './../Models/questions.json';
         $this->resultsPath = __DIR__ . './../Models/results.json';
-        $this->jsonString = file_get_contents($this->jsonPath);
+        $this->jsonString = file_get_contents($this->questionsPath);
         $this->jsonData = json_decode($this->jsonString);
-        $this->answers = $answers;
+        $this->answers = $this->setAnswersObject();
+    }
+
+    private function setAnswersObject() {
+        $answers = new stdClass();
+        $answers->question1 = $_POST["question1"];
+        $answers->question2 = $_POST["question2"];
+        $answers->question3 = $_POST["question3"];
+        $answers->question4 = $_POST["question4"];
+        $answers->question5 = $_POST["question5"];
+        return $answers;
     }
 
     private function checkAnswers() {
-        $results = array();
+        $results = new stdClass();
         foreach ($this->answers as $id => $answer) {
-            if($this->jsonData[$id] == $answer) {
-                $this->results=>[$id] == true;
+            if($this->jsonData->$id->answer == $answer) {
+                $results->$id = "true";
             } else {
-                $this->results=>[$id] == false;
+                $results->$id = "false";
             }
         }
         return $results;
--- src/Views/questions.php
+++ src/Views/questions.php
@@ -8,9 +8,17 @@
     echo 'This is the page ' . $page;
     ?>
     </p>
-    <form action="receiveAnswers" method="POST" enctype="multipart/form-data">
-        <label for="">Réponse</label>
-        <input type="text" name="test" id="test" required>
+    <form action="receiveAnswers" method="POST">
+        <label for="question1">Réponse</label>
+        <input type="text" name="question1" id="question1" required>
+        <label for="question2">Réponse</label>
+        <input type="text" name="question2" id="quesiton2" required>
+        <label for="question3">Réponse</label>
+        <input type="text" name="question3" id="questions3" required>
+        <label for="question4">Réponse</label>
+        <input type="text" name="question4" id="questions4" required>
+        <label for="question5">Réponse</label>
+        <input type="text" name="question5" id="questions5" required>
         <input type="submit" name="submit" id="submit">
     </form>
   </body>
